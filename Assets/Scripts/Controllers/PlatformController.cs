ecce6255b25ef645760912402cf9f3f788e0f9f9
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2023-07-23 11:36:49 -0600

    Fix getting stuck on the side issue

--- Assets/Scripts/Controllers/PlatformController.cs
+++ Assets/Scripts/Controllers/PlatformController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlatformController : MonoBehaviour
 {
     public float jumpForce = 10.0f;
+    public float sideBounceForce = 3.0f;
     
     public void OnCollisionEnter2D(Collision2D other)
     {
         var rb = other.collider.GetComponent<Rigidbody2D>();
         var playerController = other.collider.GetComponent<PlayerController>();
         
+        // If the player lands on the trap, then bounce a little and disable the trap
         if (gameObject.CompareTag("Trap"))
         {
             if (other.relativeVelocity.y <= 0)
@@ -31,6 +33,15 @@ public class PlatformController : MonoBehaviour
                 rb.velocity = new Vector2(0, jumpForce + playerController.GetSlamForce());
                 playerController.isSlamming = false;
             }
+        }
+        
+        // If the player collides with the side of the platform
+        if (other.gameObject.CompareTag("Player") && Mathf.Abs(other.contacts[0].normal.y) < 0.1f)
+        {
+            // Reflect the player's velocity to simulate bouncing off the side
+            var normal = other.contacts[0].normal;
+            var reflectedVelocity = Vector2.Reflect(rb.velocity, normal).normalized;
+            rb.velocity = reflectedVelocity * sideBounceForce;
         }
     }
 }
